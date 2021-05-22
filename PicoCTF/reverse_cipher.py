d5053e0f573c54c44879f5afbdd4fd1164ea253f
yogalovesalmon <81376285+yogalovesalmon@users.noreply.github.com>
2021-05-22 18:38:30 +0800

    Add files via upload

---
+++ PicoCTF/reverse_cipher.py
@@ -0,0 +1,13 @@
+cipher = 'w1{1wq83k055j5f'
+flag = ""
+
+i = 0
+for count in range(8,23):
+        if ((count & 1) == 0):
+                flag += chr(ord(cipher[i])-5)
+        else:
+                flag += chr(ord(cipher[i])+2)
+        i+=1
+
+print("picoCTF{" + flag + "}")
+
